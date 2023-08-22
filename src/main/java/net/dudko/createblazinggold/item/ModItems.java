64672901fce0ed1687a96d570f12cd706b030b03
Dudek <f.j.kowalik@gmail.com>
2023-08-22 21:10:44 +0200

    actually added burning gold sheet add incomplete burning gold ingot add recipes

--- src/main/java/net/dudko/createblazinggold/item/ModItems.java
+++ src/main/java/net/dudko/createblazinggold/item/ModItems.java
@@ -3,7 +3,6 @@ package net.dudko.createblazinggold.item;
 import net.dudko.createblazinggold.CreateBlazingGold;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -14,6 +13,10 @@ public class ModItems {
     public static final Item MAGMAGOLD_INGOT = registerItem("magmagold_ingot",
             new Item(new FabricItemSettings().group(ModItemGroup.CREATE_BLAZING_GOLD)));
     public static final Item BURNING_GOLD_INGOT = registerItem("burning_gold_ingot",
+            new Item(new FabricItemSettings().group(ModItemGroup.CREATE_BLAZING_GOLD)));
+    public static final Item INCOMPLETE_BURNING_GOLD_INGOT = registerItem("incomplete_burning_gold_ingot",
+            new Item(new FabricItemSettings()));
+    public static final Item BURNING_GOLD_SHEET = registerItem("burning_gold_sheet",
             new Item(new FabricItemSettings().group(ModItemGroup.CREATE_BLAZING_GOLD)));
 
 
